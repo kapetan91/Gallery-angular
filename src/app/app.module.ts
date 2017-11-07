5b550f03dd764084e3e4771e7c62d2850a405355
kapetan91 <jankovicsrb@gmail.com>
2017-11-07 22:06:12 +0100

    male ispravke

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { GalleryComponent } from './components/galleries/gallery/gallery.compone
 import { GalleryViewComponent } from './components/galleries/gallery-view/gallery-view.component';
 import { GalleryStoreComponent } from './components/galleries/gallery-store/gallery-store.component';
 import { FilterComponent } from './components/filter/filter.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,10 @@ import { FilterComponent } from './components/filter/filter.component';
     FilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,29 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { FilterComponent } from '../components/filter/filter.component';
 import { AuthService } from '../service/auth.service';
-import { AuthGuard } from './guards/auth.guard';
-import { GuestGuard } from './guards/guest.guard';
-import { CommentStoreComponent } from '../components/comments/comment-store/comment-store.component';
-import { GalleryStoreComponent } from '../components/galleries/gallery-store/gallery-store.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClient,
     FormsModule
   ],
-  declarations: [FilterComponent],
+  declarations: [],
   providers: [
   	AuthService,
-  	AuthGuard,
-  	GuestGuard,
-  	CommentStoreComponent,
-  	GalleryStoreComponent
+    HttpClient
   ],
   	exports: [
-  	FilterComponent,
   	FormsModule
   ]
 })
